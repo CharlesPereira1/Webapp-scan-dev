17bd720a250b92c26b9d41b7518585ae1dff73eb
Charles Pereira <54192694+CharlesPereira1@users.noreply.github.com>
2021-01-30 22:12:03 -0300

    configure eslint and .env

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-function App() {
-  return <h1>hello word</h1>;
-}
+import GlobalStyle from './styles/global.js';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>hello word</h1>
+      <GlobalStyle />
+    </>
+  );
+};
 
 export default App;
